bd35c61aacda9f8663f975126fe2167b62e1f1b7
= <=>
2023-10-03 18:51:29 +0530

    :DSF

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,13 @@
+import Footer from "../Components/Footer"
+import Navbar from "../Components/Navbar"
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default Home
